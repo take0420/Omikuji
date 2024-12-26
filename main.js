4d0611886ebacc0327d6101eb74c9dd969f0bf9d
take0420 <zakisen81@gmail.com>
2024-12-26 23:14:51 +0900

    refactor: 運勢の要素を配列に格納

--- main.js
+++ main.js
@@ -5,21 +5,9 @@
   const result = document.getElementById('result');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 4);
+    const results = ['大吉', '中吉', '小吉', '凶'];
+    const n = Math.floor(Math.random() * results.length);
 
-    switch (n) {
-      case 0:
-        result.textContent = '大吉';
-        break;
-      case 1:
-        result.textContent = '中吉';
-        break;
-      case 2:
-        result.textContent = '小吉';
-        break;
-      case 3:
-        result.textContent = '凶';
-        break;
-    }
+    result.textContent = results[n];
   });
 }
